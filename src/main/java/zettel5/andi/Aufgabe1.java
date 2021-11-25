4bee72528cc0795c1a85e36d24eab037302812b3
Andre <andi.hofmann@t-online.de>
2021-11-25 20:46:37 +0100

    25.11.2021 20:46 Uhr

--- src/main/java/zettel5/andi/Aufgabe1.java
+++ src/main/java/zettel5/andi/Aufgabe1.java
@@ -7,15 +7,13 @@ public class Aufgabe1 {
     }
 
     static void testAll() {
-        float[] test = new float[4];
-        test[0] = 13.37f;
-        test[1] = 4.20f;
-        test[2] = 6.66f;
-        test[3] = 6.9f;
+        float[] test = {13.37f, 4.2f, 6.66f, 6.9f};
+        float[] test2 = {4.2f, 6.66f, 6.9f, 13.37f};
+        float[] test3 = {13.37f, 6.9f, 6.66f, 4.2f};
 
-        float minimum1 = 4.20f;
+        float minimum1 = 4.2f;
         float maximum1 = 13.37f;
-        float average1 = 25.955f;
+        float average1 = 15.565f;
 
         float minimum2 = getMinimum(test);
         float maximum2 = getMaximum(test);
@@ -24,33 +22,95 @@ public class Aufgabe1 {
         testAllHelp(minimum1, minimum2, "getMinimum");
         testAllHelp(maximum1, maximum2, "getMaximum");
         testAllHelp(average1, average2, "calculateAverage");
+
+        testAllHelpSort(isSorted(test, true), true);
+        testAllHelpSort(isSorted(test, false), false);
+        testAllHelpSort(isSorted(test2, true), true);
+        testAllHelpSort(isSorted(test3, false), false);
     }
 
     static void testAllHelp(float f1, float f2, String testName) {
         if (f1 == f2) {
-            System.out.println("Dein Test \"" + testName + "\" war erfolgreich.");
+            System.out.println("Your test \"" + testName + "\" was successful. Your number is: " + f2);
         } else {
-            System.out.println("Dein Test \"" + testName + "\" war nicht erfolgreich.");
+            System.out.println("Your test \"" + testName + "\" failed.");
+        }
+    }
+
+    static void testAllHelpSort(boolean result, boolean ascending) {
+        if (result && ascending) {
+            System.out.println("Your test \" isSorted \" was successful. Your array is in ascending order.");
+        } else if (result && !ascending) {
+            System.out.println("Your test \" isSorted \" was successful. Your array is in descending order.");
+        } else if (!result && ascending) {
+            System.out.println("Your test \" isSorted \" failed. Your array is not in order or try descending.");
+        } else if (!result && !ascending) {
+            System.out.println("Your test \" isSorted \" failed. Your array is not in order or try ascending.");
         }
     }
 
     static float getMinimum(float[] numbers) {
-        float number = 0;
-        return number;
+        float result = numbers[0];
+        int i = 1;
+        while (i < numbers.length) {
+            if (numbers[i] < result) {
+                result = numbers[i];
+            }
+            i++;
+        }
+        return result;
     }
 
     static float getMaximum(float[] numbers) {
-        float number = 0;
-        return number;
+        float result = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > result) {
+                result = numbers[i];
+            }
+        }
+        return result;
     }
 
     static float calculateAverage(float[] numbers) {
-        float number = 0;
-        return number;
+        float result = 0;
+        for (float number : numbers) {
+            result += number;
+        }
+        result /= 2;
+        return result;
     }
 
     static boolean isSorted(float[] numbers, boolean ascending) {
+        if (ascending) {
+            return isSortedAscending(numbers, 0);
+        } else {
+            return isSortedDescending(numbers, 0);
+        }
+    }
+
+    static boolean isSortedAscending(float[] numbers, int index) {
         boolean condition = false;
+        if (index < numbers.length - 1) {
+            if (numbers[index] < numbers[index + 1]) {
+                index++;
+                condition = isSortedAscending(numbers, index);
+            }
+        } else {
+            condition = true;
+        }
+        return condition;
+    }
+
+    static boolean isSortedDescending(float[] numbers, int index) {
+        boolean condition = false;
+        if (index < numbers.length - 1) {
+            if (numbers[index] > numbers[index + 1]) {
+                index++;
+                condition = isSortedDescending(numbers, index);
+            }
+        } else {
+            condition = true;
+        }
         return condition;
     }
 }
--- src/main/java/zettel5/andi/Aufgabe2.java
+++ src/main/java/zettel5/andi/Aufgabe2.java
@@ -1,4 +1,110 @@
 package zettel5.andi;
 
+import static java.util.Arrays.copyOfRange;
+
 public class Aufgabe2 {
+    public static void main(String[] args) {
+        double[] gps = {-20.0, 0.0, 200.0, -18.5, -0.647, 200.577, -16.85, -1.237,
+                201.16, -15.035, -1.763, 201.739, -13.038, -2.219,
+                202.299, -10.842, -2.599, 202.824, -8.426, -2.894,
+                203.289, -5.769, -3.096, 203.667, -2.846, -3.195, 203.918,
+                0.369, -3.182, 203.998, 3.861, -3.205, 203.85, 7.284,
+                -3.176, 203.469, 10.638, -3.209, 202.868, 13.926, -3.175,
+                202.06, 17.147, -3.209, 201.059, 20, -3.174, 199.877};
+
+        double[] start1 = {-20.0, 0.0, 200.0};
+        double[] end1 = {-5.769, -3.096, 203.667};
+
+        double[] end3 = {-20.0, 0.0, 200.0};
+        double[] start3 = {-5.769, -3.096, 203.667};
+
+        double[] start2 = {500, 60, 700};
+        double[] end2 = {800, 90, 1000};
+
+        System.out.println(distance(gps));
+        System.out.println(velocity(gps));
+        System.out.println(maxVelocity(gps));
+
+        testPartialGPS(gps, start1, end1);
+        System.out.println("________________________________________________________________________________________");
+        testPartialGPS(gps, start2, end2);
+        System.out.println("________________________________________________________________________________________");
+        testPartialGPS(gps, start3, end3);
+        System.out.println("________________________________________________________________________________________");
+        testPartialGPS(gps, null, end3);
+
+    }
+
+    static void testPartialGPS(double[] gps, double[]start, double[]end){
+        double[] result = partialGPS(gps, start, end);
+        for (double element : result) {
+            System.out.println(element);
+        }
+    }
+
+    static double distance(double[] gps) {
+        double result = 0;
+        for (int i = 0; i < gps.length - 3; i += 3) {
+            result += Math.sqrt(Math.pow(Math.abs(gps[i] - gps[i + 3]), 2) + Math.pow(Math.abs(gps[i + 1] - gps[i + 4]), 2) +
+                    Math.pow(Math.abs(gps[i + 2] - gps[i + 5]), 2));
+        }
+        return result;
+    }
+
+    static double velocity(double[] gps) {
+        return distance(gps) / (gps.length / 3 - 1);
+    }
+
+    static double maxVelocity(double[] gps) {
+        int counter = 0;
+        double[] velocities = new double[gps.length / 3 - 1];
+        for (int i = 0; i < gps.length - 3; i += 3) {
+            velocities[counter] = Math.sqrt(Math.pow(Math.abs(gps[i] - gps[i + 3]), 2) + Math.pow(Math.abs(gps[i + 1] - gps[i + 4]), 2) +
+                    Math.pow(Math.abs(gps[i + 2] - gps[i + 5]), 2));
+            counter++;
+        }
+        return searchMaxVelocity(velocities);
+    }
+
+    static double searchMaxVelocity(double[] velocities) {
+        double result = 0;
+        for (double velocity : velocities) {
+            if (velocity > result) {
+                result = velocity;
+            }
+        }
+        return result;
+    }
+
+    static double[] partialGPS(double[] gps, double[] start, double[] end) {
+        double[] result;
+        if (start == null || end == null || !searchPartialGPS(start[0], gps) || !searchPartialGPS(start[1], gps) || !searchPartialGPS(start[2], gps)
+                || !searchPartialGPS(end[0], gps) || !searchPartialGPS(end[1], gps) || !searchPartialGPS(end[2], gps)) {
+            return gps;
+        } else {
+            int startIndex = 0;
+            int endIndex = 0;
+            for (int i = 0; start[0] != gps[i]; i++) {
+                startIndex = i;
+            }
+            for (int i = 0; end[2] != gps[i]; i++) {
+                endIndex = i;
+            }
+            if (startIndex > endIndex) {
+                return gps;
+            }
+            result = copyOfRange(gps, startIndex, endIndex);
+        }
+        return result;
+    }
+
+    static boolean searchPartialGPS(double value, double[] gps) {
+        boolean conditition = false;
+        for (double c : gps) {
+            if (value == c) {
+                conditition = true;
+            }
+        }
+        return conditition;
+    }
 }
