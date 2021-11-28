e94cc60f2620d7b9d3646803081b319cea8cb5c4
qStivi <stephanglaue@gmail.com>
2021-11-28 16:35:46 +0100

    Completed Aufgabe2
    I think I'm finished now...
    
    Signed-off-by: qStivi <stephanglaue@gmail.com>

--- src/main/java/zettel5/qStivi/Aufgabe2.java
+++ src/main/java/zettel5/qStivi/Aufgabe2.java
@@ -25,47 +25,77 @@ public class Aufgabe2 {
         System.out.println(distance(gps));
         System.out.println(velocity(gps));
         System.out.println(maxVelocity(gps));
-        System.out.println(distance(new double[]{1, 1, 1, 2, 2, 2, 3, 3, 3})); // 3,464101615137755
-        System.out.println(velocity(new double[]{1, 1, 1, 2, 2, 2, 3, 3, 3})); // 1,154700538379252
-        System.out.println(maxVelocity(new double[]{1, 1, 1, 2, 2, 2, 3, 3, 3})); // 1,154700538379252
+
+        System.out.println("-----------------------------------------------------------------");
+
+        test("distance() #1", distance(new double[]{0, 1, 0, 0, 2, 0, 0, 3, 0}), 2d);
+        test("distance() #2", distance(new double[]{0, 1, 0, 0, -2, 0, 0, 3, 0}), 8d);
+        test("distance() #2", distance(new double[]{3}), -1d);
+        test("distance() #3", distance(new double[9]), 0d);
+        test("distance() #4", distance(new double[]{}), -1d);
+
+        test("velocity() #1", velocity(new double[]{1, 0, 0, 2, 0, 0, 3, 0, 0}), 1d);
+        test("velocity() #2", velocity(new double[]{1, 0, 0, -2, 0, 0, -3, 0, 0}), 2d);
+        test("velocity() #2", velocity(new double[]{3}), -1d);
+        test("velocity() #3", velocity(new double[9]), 0d);
+        test("velocity() #4", velocity(new double[]{}), -1d);
+
+        test("maxVelocity() #1", maxVelocity(new double[]{0, 0, 0, 0, 0, 2, 0, 0, 3}), 2d);
+        test("maxVelocity() #2", maxVelocity(new double[]{0, 0, 0, 0, 0, -2, 0, 0, -3}), 2d);
+        test("maxVelocity() #3", velocity(new double[]{3}), -1d);
+        test("maxVelocity() #4", velocity(new double[9]), 0d);
+        test("maxVelocity() #5", velocity(new double[]{}), -1d);
     }
 
-    private static double distance(double[] gps) {
-        if (gps.length % 3 != 0) return -1; // Abort if coordinates are in the wrong format
-        var total = 0.0;
+    private static double[][] convertToVectorArray(double[] gps) {
         var vectors = new double[gps.length / 3 - 1][3]; // Create array of vector3s
         // We want the dintance of AB BC CD DE ...
         for (int i = 0, j = 0; i < gps.length / 3 - 1; i++) { // Iterate through every vector
             vectors[i] = Arrays.copyOfRange(gps, j, j + 6); // A vector consists of two points
             j += 3; // Next point
         }
+        return vectors;
+    }
+
+    private static double distance(double[] gps) {
+        if (gps.length % 3 != 0 || gps.length == 0) return -1; // Abort if coordinates are in the wrong format. No points have no distance.
+        var total = 0.0;
         // calculate length of every vetor and add it to the total
-        for (double[] point : vectors) {
+        for (double[] point : convertToVectorArray(gps)) {
             total += Math.sqrt(Math.pow(point[3] - point[0], 2) + Math.pow(point[4] - point[1], 2) + Math.pow(point[5] - point[2], 2));
         }
         return total;
     }
 
     private static double velocity(double[] gps) {
-        var numOfPoints = gps.length / 3;
-        return distance(gps) / numOfPoints;
+        if (gps.length % 3 != 0 || gps.length == 0) return -1; // Abort if coordinates are in the wrong format. No points have no distance.
+        return distance(gps) / ((gps.length / 3f) - 1); // Divide distance with time
     }
 
     private static double maxVelocity(double[] gps) { // Aka greates distance
         if (gps.length % 3 != 0) return -1; // Abort if coordinates are in the wrong format
         var vectorLengths = new double[gps.length / 3 - 1]; // Create array to store all lenthgs of the arrays
-        var vectors = new double[gps.length / 3 - 1][3]; // Create array of vector3s
-        // We want the dintance of AB BC CD DE ...
-        for (int i = 0, j = 0; i < gps.length / 3 - 1; i++) { // Iterate through every vector
-            vectors[i] = Arrays.copyOfRange(gps, j, j + 6); // A vector consists of two points
-            j += 3; // Next point
-        }
+        var vectors = convertToVectorArray(gps);
         // calculate length of every vetor and add it to the total
         for (int i = 0; i < vectors.length; i++) {
             double[] vector = vectors[i];
             vectorLengths[i] = Math.sqrt(Math.pow(vector[3] - vector[0], 2) + Math.pow(vector[4] - vector[1], 2) + Math.pow(vector[5] - vector[2], 2));
         }
-        Arrays.sort(vectorLengths);
-        return vectorLengths[vectorLengths.length - 1];
+        // Copied from getMaximum()
+        var max = Double.MAX_VALUE * -1; // Very small value.
+        for (double vectorLength : vectorLengths) {
+            if (vectorLength > max) max = vectorLength; // Compare to current maximum value and set new value when current is smaller.
+        }
+        return max;
+    }
+
+    private static void test(String name, Object calculated, Object expected) {
+        if (calculated.equals(expected)) {
+            System.out.println(name + " passed.");
+        } else {
+            System.out.println(name + " failed!");
+            System.out.println("Expected: " + expected + " But got: " + calculated);
+        }
+        System.out.println("-----------------------------------------------------------------");
     }
 }
