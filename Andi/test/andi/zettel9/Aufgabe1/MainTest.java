9f8a53b92e5da5135cdd77e17bab51a383782a49
Andii01 <andi.hofmann@t-onlie.de>
2022-01-20 14:38:55 +0100

    20.1 14:38 Uhr

--- Andi/test/andi/zettel9/Aufgabe1/MainTest.java
+++ Andi/test/andi/zettel9/Aufgabe1/MainTest.java
@@ -1,16 +1,26 @@
 package andi.zettel9.Aufgabe1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
     void replace() {
+        String a1 = "Da steht ein rotes Auto und ein blaues Auto.";
+        String b1 = "Da steht ein rotes Spielzeug und ein blaues Spielzeug.";
+        Assertions.assertEquals(b1, Main.replace(a1, "Auto", "Spielzeug"));
+
+        String a2 = "Noch ein Satz, um die Methode auszutesten.";
+        String b2 = "Noch ein Satz, um die Methode au$zute$ten.";
+        Assertions.assertEquals(b2, Main.replace(a2, "s", "$"));
     }
 
     @Test
     void isReverse() {
+        Assertions.assertTrue(Main.isReverse("Frühstück", "Kcütshürf"));
+        Assertions.assertTrue(Main.isReverse("Hallo", "Ollah"));
+        Assertions.assertFalse(Main.isReverse("Wahr", "Falsch"));
     }
 }
